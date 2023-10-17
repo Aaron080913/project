1b6cc6597051d0d2698b3b0edba1c2a88d4bb8ef
Aaron080913 <>
2023-10-17 21:14:41 +0000

    added rock paper scissors buttons

---
+++ assets/JS/index.js
@@ -0,0 +1,15 @@
+const playGame = () => {
+    let playerScore = 0;
+    let computerScore = 0;
+    let moves = 0;
+
+    // function to
+    const playGame = () => {
+        const rockBtn = document.querySelector('rock');
+        const paperBtn = document.querySelector('paper');
+        const scissorBtn = document.querySelector('scissors');
+        const playerOptions = [rockBTN, paperBtn, scissorBtn];
+        const computerOptions = ['rock','paper','scissors']
+
+    }
+}
