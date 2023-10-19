076421769a4064c863101fe258846c62bd23c05a
Aaron080913 <>
2023-10-19 19:23:03 +0000

    added java to play game and to check winner

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -5,22 +5,35 @@ const playGame = () => {
 
     // function to
     const playGame = () => {
-        const rockBtn = document.querySelector('rock');
-        const paperBtn = document.querySelector('paper');
-        const scissorBtn = document.querySelector('scissors');
+        const rockBtn = document.querySelector('.rock');
+        const paperBtn = document.querySelector('.paper');
+        const scissorBtn = document.querySelector('.scissors');
         const playerOptions = [rockBTN, paperBtn, scissorBtn];
         const computerOptions = ['rock','paper','scissors']
 
         // to start playing the game
         playerOptions.forEach(option => {
             option.addEventListener('click', function(){
-                const movesLeft = document.querySelector('.movesleft'); moves++;
+                const movesLeft = document.querySelector('.moves-left'); moves++;
                 movesLeft.innerText = 'Moves Left: ${10-moves}';
 
                 const choiceNumber = Math.floor(Math.random()*3);
                 const computerchoice = computerOptions[choiceNumber];
+
+                // check who wins
+                winner(this.innerText,computerchoice)
+
+                //game over after 10 moves
+                if(moves == 10){
+                    gameOver(playerOptions,moves-left);
+                }
             })
         })
 
     }
+}
+
+// decide who the winner is
+const winner = (player,computer) => {
+    
 }
